7310b8692bba54859dec5bff4baf823fab90c0d9
zhangyu <zhangyu4687@haixue.com>
2024-02-27 01:35:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# Web3 Discord Tools
+
+- [x] Avail 领水
+- [x] Babylon 领水
+- [ ] ...
+
+## 安装
+```
+yarn install
+```
+
+## 领水
+> 当前仅支持`avail`与`babylon`领水
+
+```
+yarn cli faucet [project] -t [discord-token] -a [wallet-address]
+```
+支持`--headless`参数
+
+# 原作者：https://github.com/xzone911/web3-discord
+首先感谢开源分享。在原作者的基础上改了bbl的批量领水
+```angular2html
+使用方式：
+1: 配置好 faucets.json ，其中
+execInterval：30000 // 时间间隔，每一次发送命令后的下个号的发送间隔时间
+tokenMapArrays：[
+    "token1=address1",
+    "token2=address2"   // 领水的discord token 中间以=分割 bbl的地址
+]
+
+2: 启动命令：yarn cli faucet babylon_auto
+
+目前bbl的dc领水是6小时一次，理论上一天能领4次。
+```
---
+++ src/scripts/faucet.ts
@@ -0,0 +1,101 @@
+import Puppet from "../puppet"
+import {sayFaucetLog} from "../utils/promots"
+import {readFile} from "fs/promises"
+import PuppetOptions from "../utils/PuppetOptions";
+import * as trace_events from "trace_events";
+
+interface FaucetOptions {
+    token: string,
+    account: string,
+    headless?: boolean
+}
+
+export const runFaucet = async (project: string, options: FaucetOptions) => {
+    sayFaucetLog()
+    //if (!options.token) throw new Error("Discord token not set!")
+    //if (!options.account) throw new Error("Faucet target address not set!")
+    const str = await readFile("./faucets.json", "utf-8")
+    const faucets = JSON.parse(str) as Record<string, {
+        name: string,
+        serverId: string,
+        channelId: string,
+        type: string,
+        cycle: number,
+        arg1: string,
+        args: string[],
+        tokenMapArrays: string[],
+        execInterval: number
+    }>
+    const faucetInfo = faucets[project]
+    if (!faucetInfo) {
+        throw new Error(`Faucet attempt failed: 'project' ${project} is not found in faucets.json.`)
+    }
+    const {
+        name, serverId, channelId, type,
+        cycle, arg1, args, tokenMapArrays, execInterval
+    } = faucetInfo
+    function sleep(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    // const {token, account, headless} = options
+    // const puppet = new Puppet(PuppetOptions(token, headless))
+    // await puppet.start()
+    // await puppet.goToChannel(serverId, channelId)
+    if (type === 'msg') {
+        // puppet.sendMessage(arg1 + ' ' + account)
+        // setInterval(() => {
+        //     puppet.sendMessage(arg1 + ' ' + account)
+        // }, cycle * 1000)
+
+        // 支持bbl
+        if (name === "bbl") {
+            for (let tokenStr of tokenMapArrays) {
+                try {
+                    let tokenArr = tokenStr.split("=");
+                    let tmpToken = tokenArr[0]
+                    let bblAddress = tokenArr[1]
+
+                    console.log(`[use token]: ${tmpToken} `, new Date())
+                    const puppet = new Puppet(PuppetOptions(tmpToken, true))
+                    await puppet.start()
+                    await puppet.goToChannel(serverId, channelId)
+                    await puppet.sendCommand(arg1, bblAddress);
+                    await puppet.closeBrowser()
+
+                    console.log(`[execInterval]: next execution will be in ${execInterval} ms.....`)
+                    await sleep(execInterval);
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+        }
+
+    } /*else {
+        if (arg1 != null) {
+            puppet.sendCommand(arg1, account);
+            setInterval(() => {
+                puppet.sendCommand(arg1, account);
+            }, cycle * 1000);
+        } else if (Array.isArray(args)) {
+            let delay = 0;
+            args.forEach(arg => {
+                setTimeout(() => {
+                    puppet.sendCommand(arg, account);
+                }, delay);
+                delay += 10000;
+            });
+            setInterval(() => {
+                let delay = 0;
+                args.forEach(arg => {
+                    setTimeout(() => {
+                        puppet.sendCommand(arg, account);
+                    }, delay);
+                    delay += 10000;
+                });
+            }, cycle * 1000);
+        } else {
+            throw new Error(`${project} args or arg1 not found.`);
+        }
+    }*/
+
+}
